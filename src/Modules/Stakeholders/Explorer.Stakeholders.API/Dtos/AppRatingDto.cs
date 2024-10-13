2946e84777cb2228ca828d20750733f9b6fe0e66
Nikola Andric <nikolandric123@gmail.com>
2024-10-13 14:56:07 +0200

    feat: Created Dto class, Auto mapper and implemented service class

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/AppRatingDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.Stakeholders;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    public class AppRatingDto
+    {
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime TimeCreated { get; set; }
+        
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/AppRating.cs
@@ -0,0 +1,28 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class AppRating : Entity
+    {
+        public int Rating { get; private set; }
+        public string ?Comment { get; private set; }
+        public DateTime TimeCreated {  get; private set; }
+        public User UserPosted {  get; private set; }
+        public int UserPostedId {  get; private set; }
+
+        public AppRating(int rating, string comment, DateTime timeCreated, int userPosted)
+        {
+            Rating = rating;
+            Comment = comment;
+            UserPostedId = userPosted;
+            TimeCreated = timeCreated;
+        }
+
+
+    }
+}

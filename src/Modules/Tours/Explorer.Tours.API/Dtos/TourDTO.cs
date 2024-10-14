7c15feb7be5c801d5eb5acd27223d098d65c4b08
Mikuta02 <jakovljevicm002@gmail.com>
2024-10-14 21:32:02 +0100

    feat: Implemented integration tests for TourController and updated test data scripts

--- src/Modules/Tours/Explorer.Tours.API/Dtos/TourDTO.cs
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourDTO.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Explorer.Tours.API.Dtos
+﻿namespace Explorer.Tours.API.Dtos
 {
     public class TourDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Weight { get; set; }

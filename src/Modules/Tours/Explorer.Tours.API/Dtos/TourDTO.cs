3f63b1b2ed5ec3db6ff9e513a26c6395c185f2ef
Mikuta02 <jakovljevicm002@gmail.com>
2024-10-14 19:44:11 +0100

    feat: implement Tour service with DTO and CRUD operations

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class TourDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Weight { get; set; }
+        public string[] Tags { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/TourService.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.Core.Domain;
+
+namespace Explorer.Tours.Core.UseCases.Administration
+{
+    public class TourService : CrudService<TourDTO, Tour>
+    {
+        public TourService(ICrudRepository<Tour> repository, IMapper mapper) : base(repository, mapper) { }
+
+    }
+}

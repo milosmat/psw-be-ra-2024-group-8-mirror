c099bb6b056a76dc6fddfc7586924281cfc88720
milosmat <shomikv22@gmail.com>
2024-10-15 19:18:39 +0200

    transitioning to a newer version

---
+++ src/Explorer.API/Controllers/Administrator/Administration/TourCheckpointController.cs
@@ -0,0 +1,48 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Administrator.Administration
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/author/tour-checkpoints")]
+    public class TourCheckpointController : BaseApiController
+    {
+        private readonly ITourCheckpointService _tourCheckpointService;
+
+        public TourCheckpointController(ITourCheckpointService tourCheckpointService)
+        {
+            _tourCheckpointService = tourCheckpointService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<TourCheckpointDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _tourCheckpointService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<TourCheckpointDto> Create([FromBody] TourCheckpointDto tourCheckpoint)
+        {
+            var result = _tourCheckpointService.Create(tourCheckpoint);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<TourCheckpointDto> Update([FromBody] TourCheckpointDto tourCheckpoint)
+        {
+            var result = _tourCheckpointService.Update(tourCheckpoint);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _tourCheckpointService.Delete(id);
+            return CreateResponse(result);
+        }
+    }
+}

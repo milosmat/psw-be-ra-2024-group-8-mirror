9217b3921b60a40fdbc34dff8be5878052646968
andjelavukosav <andjelavukosav3@gmail.com>
2024-10-15 17:16:35 +0200

    feat : Implementation of Dto and service classes needed to solve the functionality of Creating a blog

---
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/BlogsDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.API.Dtos
+{
+    public class BlogsDto
+    {
+        public int Id { get; set; }
+        public string Description { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        public List<string>? Images { get; private set; }
+        public BlogsStatus Status { get; private set; }
+    }
+    public enum BlogsStatus
+    {
+        Draft,
+        Published,
+        Closed
+    }
+}

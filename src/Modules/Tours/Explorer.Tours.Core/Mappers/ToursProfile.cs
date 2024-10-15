d51cd28a5270d1792d9034ea2127f29961f8658c
Biljana <biljanakatanic7@gmail.com>
2024-10-16 01:21:39 +0200

    feat: Creating the controller and configuring the infrastructure for the tour preference setting functionality
    fix: Corrected issues related to the mapping of , including adjustments to dictionary and list handling.

--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.Core.Domain;
+using System.Text.Json;
+using TransportMode = Explorer.Tours.API.Dtos.TransportMode;
 
 namespace Explorer.Tours.Core.Mappers;
 
@@ -9,5 +11,11 @@ public class ToursProfile : Profile
     public ToursProfile()
     {
         CreateMap<EquipmentDto, Equipment>().ReverseMap();
+        CreateMap<TourPreferencesDto, TourPreferences>().ReverseMap();
+
+        CreateMap<TourPreferencesDto, TourPreferences>()
+            .ForMember(dest => dest.TransportPreferences, opt => opt.MapFrom(src => src.TransportPreferences))
+            .ForMember(dest => dest.InterestTags, opt => opt.MapFrom(src => src.InterestTags))
+            .ReverseMap();
     }
 }
--- src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
@@ -1,16 +1,34 @@
 ﻿using Explorer.Tours.Core.Domain;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace Explorer.Tours.Infrastructure.Database;
 
 public class ToursContext : DbContext
 {
     public DbSet<Equipment> Equipment { get; set; }
+    public DbSet<TourPreferences> TourPreferences { get; set; }  
 
     public ToursContext(DbContextOptions<ToursContext> options) : base(options) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("tours");
+
+        // Mapping for Dictionary<TransportMode, int>
+        modelBuilder.Entity<TourPreferences>()
+        .Property(p => p.TransportPreferences)
+        .HasConversion(
+            v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null), 
+            v => JsonSerializer.Deserialize<Dictionary<TransportMode, int>>(v, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) 
+        );
+
+        // Mapping for List<string>
+        modelBuilder.Entity<TourPreferences>()
+            .Property(p => p.InterestTags)
+            .HasConversion(
+                v => string.Join(',', v), 
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList() 
+            );
     }
 }

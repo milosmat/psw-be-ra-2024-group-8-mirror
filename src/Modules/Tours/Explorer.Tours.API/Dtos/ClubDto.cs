4210a3f98a674679df7e40126695e76926c07bd4
vecca49 <vecaradovic49@gmail.com>
2024-10-16 21:46:05 +0200

    Implemented ClubDto and ClubService

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/ClubDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos;
+
+public class ClubDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Photo { get; set; }
+    public int OwnerId { get; set; }
+}

c290dac8514dddb26c0c8f2030871e6708b1d73b
ana.tamindzija <tamindzijaana7@gmail.com>
2024-10-16 02:57:49 +0200

    feat: Configuring DbContext and the dependency injection system.

---
+++ src/Explorer.API/Controllers/Tourist/CommentController.cs
@@ -0,0 +1,48 @@
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/tourist/comment")]
+    public class CommentController : BaseApiController
+    {
+        private readonly ICommentService _commentService;
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<CommentDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _commentService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<CommentDto> Create([FromBody] CommentDto comment)
+        {
+            var result = _commentService.Create(comment);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<CommentDto> Update([FromBody] CommentDto comment)
+        {
+            var result = _commentService.Update(comment);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _commentService.Delete(id);
+            return CreateResponse(result);
+        }
+    }
+}

bda7956edb2954e693f2dbf850f7991c5e7560bc
gozden2001 <borisgozdenovic@gmail.com>
2024-10-16 18:37:29 +0200

    feat: implement object model

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Object.cs
@@ -0,0 +1,33 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    internal class Object : Entity
+    {
+        public String Name { get; init; }
+        public String Description { get; init; }
+        public String Image { get; private set; }
+        public ObjectCategory Category { get; private set; }
+
+
+        public Object(string name, string description, string image, ObjectCategory objectCategory)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Invalid Name.");
+            Name = name;
+            Description = description;
+            Image = image;
+            Category = objectCategory;
+        }
+    }
+}
+
+public enum ObjectCategory
+{
+    TOILET, PARKING, RESTAURANT, OTHER
+}

099e865f73c3ae5796646c8e1502b81b17294aa1
milenaa10 <148877412+milenaa10@users.noreply.github.com>
2024-11-13 00:20:39 +0100

    Implementacija funkcionalnosti za shopping cart i token servise

---
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/Repositories/CardDataBaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Infrastructure.Database;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
+using Explorer.Tours.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Explorer.Tours.Infrastructure.Database.Repositories
+{
+    public class CardDataBaseRepository : CrudDatabaseRepository<ShoppingCart, ToursContext>, ICardRepository
+    {
+        public CardDataBaseRepository(ToursContext dbContext) : base(dbContext)
+        {
+        }
+
+        public new ShoppingCart? Get(long id)
+        {
+            return DbContext.ShoppingCard.Where(t => t.Id == id)
+                .Include(t => t.ShopingItems!).FirstOrDefault();
+        }
+
+        public ShoppingCart Update(ShoppingCart aggregateRoot)
+        {
+            DbContext.Entry(aggregateRoot).State = EntityState.Modified;
+            DbContext.SaveChanges();
+            return aggregateRoot;
+        }
+
+
+        public ShoppingCart? GetByTouristId(long touristId)
+        {
+            return DbContext.ShoppingCard
+                .Where(t => t.TouristId == touristId)  // Traži po TouristId
+                .Include(t => t.ShopingItems!)
+                .FirstOrDefault();
+        }
+
+    }
+}

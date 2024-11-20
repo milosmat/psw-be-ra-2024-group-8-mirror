b6434ccfaa71d9724945658fa276c61f7cd2485b
Bogdan Djukic <bogdandjukic25@gmail.com>
2024-11-21 00:53:04 +0100

    encounters module postavka

---
+++ src/Modules/Encounters/Explorer.Encounters.Infrastructure/EncountersStartup.cs
@@ -0,0 +1,30 @@
+﻿using Explorer.Encounters.Core.Mappers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Infrastructure
+{
+    public static class EncountersStartup
+    {
+
+        public static IServiceCollection ConfigureEncountersModule(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(EncountersProfile).Assembly); // Preduslov da imamo ovu liniju koda je da smo definisali već Profile klasu u Core/Mappers
+            SetupCore(services);
+            SetupInfrastructure(services);
+            return services;
+        }
+
+        private static void SetupCore(IServiceCollection services)
+        {
+
+        }
+        private static void SetupInfrastructure(IServiceCollection services)
+        {
+        }
+    }
+}

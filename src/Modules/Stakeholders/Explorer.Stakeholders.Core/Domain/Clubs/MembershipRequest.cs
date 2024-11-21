3a3796a38e15c4a75e9dcff04f838d0148b9ba97
andjelavukosav <andjelavukosav3@gmail.com>
2024-11-21 15:16:43 +0100

    Club refactoring

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Clubs/MembershipRequest.cs
@@ -0,0 +1,39 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain.Clubs
+{
+    public class MembershipRequest : Entity
+    {
+        public int SenderId { get; private set; }
+        public int FollowerId { get; private set; }
+
+        public Status Status { get; private set; }
+        public long ClubId { get; set; }
+
+
+        public Club Club { get; set; }
+
+        public MembershipRequest() { }
+        public MembershipRequest(int senderId, int followerId, Status status)
+        {
+            SenderId = senderId;
+            FollowerId = followerId;
+            Status = status;
+        }
+    }
+
+
+    public enum Status
+    {
+        None,
+        Pending,
+        Accepted,
+        Rejected,
+        Invated
+    }
+}
